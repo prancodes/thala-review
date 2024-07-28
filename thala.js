11aa29b7074ec3bfb9e2b602b6b5c18300dfbf91
Pranjal Singh <155365177+prancodes@users.noreply.github.com>
2024-07-28 10:04:15 +0530

    Update thala.js

--- thala.js
+++ thala.js
@@ -11,7 +11,33 @@ btn.addEventListener("click", ()=>{
     }
 })
 
+function yesThala(){let btn = document.querySelector("button");
+
+btn.addEventListener("click", ()=>{
+    let word = document.querySelector("#myInput");
+    let thala = word.value; 
+    if (thala.length == 7) {
+        yesThala();
+    }
+    else{
+        console.log("no thala");
+    }
+})
+
 function yesThala(){
+    let inframe = document.querySelector("#inside_frame");
+    inframe.style.display = "none";
+
+    let frame = document.querySelector("#frame");
+    frame.style.padding = "0px 20px 40px 20px"
+
+    let videoFrame = document.querySelector("#video_frame");
+    videoFrame.style.visibility = "visible"  
+
+
+}
+
+
     let inframe = document.querySelector("#inside_frame");
     inframe.style.display = "none";
 
