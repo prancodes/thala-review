843cb22558d2a9d24d395f45136ccdb760a46f8a
Pranjal Singh <155365177+prancodes@users.noreply.github.com>
2024-08-03 16:02:54 +0530

    Update thala.js

--- thala.js
+++ thala.js
@@ -8,35 +8,19 @@ btn.addEventListener("click", ()=>{
     for (let num of thala) {
         sum = sum + Number(num);
     }
-    let arr = ['Thala','Dhoni','Mahi'];
+    let arr = ['Thala','Dhoni','Mahi','777'];
 
-    if (thala.length == 7 || sum == 7 || arr.includes(thala)
-        ) {
+    if (thala.length == 7 || sum == 7 || arr.includes(thala)) {
         thalaReview();
-        setTimeout(yesThala, 1200);
+        setTimeout(()=>{
+            yesThala(thala,sum)
+        }, 1200);
     }
     else{
         thalaReview();
         setTimeout(noThala, 1200);
     }
 })
-
-function yesThala(){
-    let inframe = document.querySelectorAll("#inside_frame, #review");
-    inframe.forEach(e =>{
-        e.style.display = "none";
-    });
-
-    let frame = document.querySelector("#frame");
-    frame.style.padding = "0px 20px 40px 20px";
-
-    let videoFrame1 = document.querySelector("#video_frame1");
-    videoFrame1.style.display = "block";
-
-    document.querySelector("#yesThala").play();
-
-    document.querySelector("#footer").style.bottom = "-30%"
-}
 
 function thalaReview() {
     let inframe = document.querySelector("#inside_frame");
@@ -46,7 +30,7 @@ function thalaReview() {
     review.style.visibility = "visible"
 }
 
-function noThala(){
+function yesThala(thala,sum){
     let inframe = document.querySelectorAll("#inside_frame, #review");
     inframe.forEach(e =>{
         e.style.display = "none";
@@ -54,6 +38,31 @@ function noThala(){
 
     let frame = document.querySelector("#frame");
     frame.style.padding = "0px 20px 40px 20px";
+    if (thala.length == 7) {
+        let text = document.querySelector("#text");
+        text.innerText = `'${thala}' has 7 Letters`;
+    }
+    else if (sum == 7) {
+        text.innerText = `Sum of all digits = 7`;
+    }
+
+    let videoFrame1 = document.querySelector("#video_frame1");
+    videoFrame1.style.display = "block";
+
+    document.querySelector("#yesThala").play();
+
+    document.querySelector("#footer").style.bottom = "-20%"
+    
+}
+
+function noThala(){
+    let inframe = document.querySelectorAll("#inside_frame, #review");
+    inframe.forEach(e =>{
+        e.style.display = "none";
+    });
+
+    let frame = document.querySelector("#frame");
+    frame.style.padding = "0px 20px 35px 20px";
 
     let videoFrame1 = document.querySelector("#video_frame1");
     videoFrame1.style.display = "none";
@@ -61,6 +70,6 @@ function noThala(){
     let videoFrame2 = document.querySelector("#video_frame2");
     videoFrame2.style.display = "block";  
     
-        document.querySelector("#footer").style.bottom = "-30%"
+    document.querySelector("#footer").style.bottom = "-20%"
 }
 
